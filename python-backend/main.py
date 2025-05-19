3c1f87cdf585d9ec7ba2e86962fd83f044b75feb
metharda <ahmetardaorak@gmail.com>
2025-05-19 15:28:52 +0300

    small bug fix

--- python-backend/main.py
+++ python-backend/main.py
@@ -1,8 +1,11 @@
 from utils.P2PNode import P2PNode
 import time
+import os
 
 if __name__ == "__main__":
     node = P2PNode()
+    if not os.path.exists("publicFiles"):
+        os.makedirs("publicFiles")
     try:
         while True:
             time.sleep(5)
